e65275db9a5c6c2e8a54889bbf0a9c4f4244faa3
Adi <119922740+adityathakur17@users.noreply.github.com>
2026-08-11 12:43:11 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        result = ''
+        for s in strs:
+            result += str(len(s)) + '#' + s
+        return result
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        i=0
+        while i<len(s):
+            j = i
+            while s[j] !='#':
+                j+=1
+            num_pos = int(s[i:j])
+            i = j+1
+            j = i+num_pos
+            s_new = s[i:j]
+            result.append(s_new)
+            i = j
+        return result
+
+
